c269c24cac30ce7dce164b4abd9693fd5761709f
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-23 17:40:25 +0000

    Complete Navbar

--- train_up/settings.py
+++ train_up/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-janisroz-pp5-bke93gxjr8e.ws-eu108.gitpod.io',
-                'train-up-pp5-da4fce5b9326.herokuapp.com'
-            ]
+                 'train-up-pp5-da4fce5b9326.herokuapp.com'
+                ]
 
 
 # Application definition
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -155,8 +156,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
