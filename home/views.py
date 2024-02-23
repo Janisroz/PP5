c57f7804dd297028338ba502cd641c366ba1604b
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-23 19:53:11 +0000

    create url and view for about us page

--- home/views.py
+++ home/views.py
@@ -7,3 +7,10 @@ def index(request):
     Returns Index Page
     """
     return render(request, 'home/index.html')
+
+
+def about_us(request):
+    """
+    Returns About us Page
+    """
+    return render(request, 'home/about-us.html')
