0a408e1b75665f59cbb878b53308779496b549b2
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-02-28 10:52:01 +0000

    Create view, url & template for product detail

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 # pylint: disable=import-error
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -15,3 +15,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    Returns a product detail page for each product
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product' : product
+    }
+
+    return render(request, 'products/product_detail.html', context)
