4c02d39b580b50931ead7debf9d4781ae70e8982
Janis Rozenbergs <janisrozkalejs@gmail.com>
2024-03-17 16:13:55 +0000

    Correct rating button and style image on sesion feed form

--- sessionfeed/forms.py
+++ sessionfeed/forms.py
@@ -8,12 +8,19 @@ class PostForm(forms.ModelForm):
     """
     Form for creating a session post
     """
-    session_rating = forms.IntegerField(
-        widget=forms.NumberInput(attrs={'class': 'form-control', 'type': 'number', 'id': 'id_session_rating'}),
-        validators=[MinValueValidator(0), MaxValueValidator(5)],
-        label='Session Rating (Must be between 1-5)'
-    )
+    # session_rating = forms.IntegerField(
+    #     widget=forms.NumberInput(attrs={'class': 'form-control', 'type': 'number', 'id': 'id_session_rating'}),
+    #     validators=[MinValueValidator(0), MaxValueValidator(5)],
+    #     label='Session Rating (Must be between 1-5)'
+    # )
+
+    # class Meta:
+    #     model = Post
+    #     fields = ('description','session','session_rating',)
 
     class Meta:
         model = Post
-        fields = ('description','session','session_rating',)
+        fields = ('description', 'session', 'session_rating',)
+        widgets = {
+            'session_rating': forms.Select(choices=[(i, i) for i in range(1, 6)]),
+        }
